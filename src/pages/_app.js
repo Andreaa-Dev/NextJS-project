9b5355c467cd860f5fa290472d785dcfc859bef8
Andrea <ngocanhhsgs@gmail.com>
2022-06-19 20:09:17 +0300

    set up and doing drawer for mobile ver

---
+++ src/pages/_app.js
@@ -0,0 +1,9 @@
+import "react-multi-carousel/lib/styles.css";
+import "react-modal-video/css/modal-video.min.css";
+import "rc-drawer/assets/index.css";
+import "typeface-dm-sans";
+
+// import everything -  can use globally
+export default function CustomApp({ Component, pageProps }) {
+  return <Component {...pageProps} />;
+}
